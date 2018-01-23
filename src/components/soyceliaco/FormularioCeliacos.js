e97c0ecefefd7fb852783b934d290e11c00bac87
benhid <antonio.b@uma.es>
2018-01-23 13:02:49 +0100

    Minor changes

--- src/components/soyceliaco/FormularioCeliacos.js
+++ src/components/soyceliaco/FormularioCeliacos.js
@@ -19,12 +19,22 @@ const log = (type) => console.log.bind(console, type);
 class QuestionsSet extends React.Component {
   render () {
     return (
-      <div>
-        <Form schema={schema}
-              onChange={log("changed")}
-              onSubmit={log("submitted")}
-              onError={log("errors")} />
-      </div>
+      <section id="soyceliaco" className="section-padding text-justify">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <h2 className="ser-title">Realizar cuestionario:</h2>
+              <hr className="botm-line"></hr>
+            </div>
+            <div className="col-md-12 col-sm-12">
+              <Form schema={schema}
+                    onChange={log("changed")}
+                    onSubmit={log("submitted")}
+                    onError={log("errors")} />
+            </div>
+          </div>
+        </div>
+      </section>
     );
   }
 }
